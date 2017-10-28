beb308937cff1a3db76d1d8e447f5de13a51f4ef
Bala M <bmp@contractor.jcp.com>
2017-10-28 06:31:11 +0530

    merge conflict addressed

--- src/js/containers/DesignSystemPage.js
+++ src/js/containers/DesignSystemPage.js
@@ -5,6 +5,7 @@ import Color from '../components/color';
 import Icons from '../components/icons';
 import Grid from '../components/grid';
 import Typography from '../components/typography';
+import ContentLoaders from '../components/content-loaders';
 import SubNav from './SubNav';
 
 class DesignSystemPage extends Component {
@@ -16,7 +17,8 @@ class DesignSystemPage extends Component {
    Color: {componentElement: Color, componentDisplayName: "Color", componentSubNav: ['Core', 'Messaging', 'Rewards', 'Marketing', 'Color Usage', 'Accessibility']},
    Icons: {componentElement: Icons, componentDisplayName: "Icons", componentSubNav: ['When to Use', 'Alert', 'Payment', 'Action', 'Navigation', 'Utility', 'Social']},
    Grid: {componentElement: Grid, componentDisplayName: "Grid", componentSubNav: ['Horizontal Grid', 'Vertical Grid', 'Documentation']},
-   Typography: {componentElement: Typography, componentDisplayName: "Typography", componentSubNav: ['Code Generator', 'Price', 'Title', 'Standard Text', 'Copy', 'Lists', 'Links', 'Promo Labels']}
+   Typography: {componentElement: Typography, componentDisplayName: "Typography", componentSubNav: ['Code Generator', 'Price', 'Title', 'Standard Text', 'Copy', 'Lists', 'Links', 'Promo Labels']},
+   ContentLoaders: {componentElement: ContentLoaders, componentDisplayName: "ContentLoaders", componentSubNav: ['Types', 'Lazy Load']}
   }
 
   const Scene = routes[this.props.componentName].componentElement;
