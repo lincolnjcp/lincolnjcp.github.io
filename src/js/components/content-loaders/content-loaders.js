a80b938fa5a8a55aa43710572829aa022a7067a1
Bala M <bmp@contractor.jcp.com>
2017-11-01 19:05:41 +0530

    lazyload comments and loader comments addressed.

--- src/js/components/content-loaders/content-loaders.js
+++ src/js/components/content-loaders/content-loaders.js
@@ -8,15 +8,17 @@ componentDidMount() {
         $(this).prop('Counter',0).animate({
             Counter: $(this).text()
         }, {
-            duration: 4000,
+            duration: 2000,
             step: function (now) {
                 $(this).text(Math.ceil(now));
             }
         });
     });
-    window.setTimeout(function(){$('.checkmark').removeClass('hide');}, 4000);
+    setTimeout(function() {$(".lazy-load-1").addClass("lazy-count");}, 500);
+    setTimeout(function() {$(".lazy-load-2").addClass("lazy-count");}, 1200);
+    setTimeout(function() {$(".lazy-load-3").addClass("lazy-count");}, 1900);
+    setTimeout(function() {$(".lazy-load-4").addClass("lazy-count");}, 2600);
 }
-
     render() {
     return (
 <div>
@@ -35,7 +37,15 @@ componentDidMount() {
             <div className="small-12 medium-12 columns">
                 <div className="small-12 medium-4 columns">
                 <div className="loader-container">
-                    <div className="loader"></div>
+                <div className="loader-spin">
+                    <div className="loader-logo">
+                        <div className="loader-circle">
+                            <svg className="loader-circle-spinner" viewBox="0 0 500 500">
+                                <circle cx="250" cy="250" r="239"> </circle>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
                 </div>
                 </div>
                 <div className="small-12 medium-8 columns">
@@ -46,7 +56,7 @@ componentDidMount() {
             <div className="small-12 medium-12 columns">
                 <div className="small-12 medium-4 columns position-rel">
                     <div className="loader-checkmark-container">
-                        <svg className="checkmark hide" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                        <svg className="checkmark" version="1.1" xmlns="http://www.w3.org/2000/svg">
                             <circle className="checkmark-circle" fill="none" cx="85.6" cy="80.6" r="30" transform="rotate(-45, 93, 93)"/>
                             <polyline className="checkmark-icon" fill="none" points="93,79.8 77.1,103.4 68,95.4"/>
                         </svg>
@@ -61,11 +71,11 @@ componentDidMount() {
                     <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec ullamcorper nulla non metus auctor fringilla. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. <br /><br /><br /><br /></p>
                 </div>
             </div>
-            <div className="small-12 medium-12 columns">
+            <div className="small-12 medium-12 columns section-container">
                 <div className="small-12 medium-4 columns">
-                    <div className="animated-background progressbar-1"></div>
-                    <div className="animated-background progressbar-2"></div>
-                    <div className="animated-background progressbar-3"></div>
+                    <div className="shimmer-bg progressbar-1"></div>
+                    <div className="shimmer-bg progressbar-2"></div>
+                    <div className="shimmer-bg progressbar-3"></div>
                 </div>
                 <div className="small-12 medium-8 columns">
                     <h4>Section</h4>
@@ -81,47 +91,47 @@ componentDidMount() {
             <h3>Lazy Load</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             <div className="lazy-load-container columns">
-                <div className="lazy-load small-6 medium-4 large-3 xlarge-3 column">
+                <div className="lazy-load small-6 medium-4 large-3 xlarge-3 column lazy-load-1">
                     <div className="lazy-border">
-                        <div className="animated-background lazy-main"></div>
+                        <div className="lazyload-bg lazyload-fade lazy-main"></div>
                         <hr/>
                         <div className="lazy-sub">
-                            <div className="animated-background progressbar-1"></div>
-                            <div className="animated-background progressbar-2"></div>
-                            <div className="animated-background progressbar-3"></div>
+                            <div className="lazyload-bg lazyload-fade progressbar-1"></div>
+                            <div className="lazyload-bg lazyload-fade progressbar-2"></div>
+                            <div className="lazyload-bg lazyload-fade progressbar-3"></div>
                         </div>
                     </div>
                 </div>
-                <div className="lazy-load small-6 medium-4 large-3 xlarge-3 column">
+                <div className="lazy-load small-6 medium-4 large-3 xlarge-3 column lazy-load-2">
                 <div className="lazy-border">
-                    <div className="animated-background lazy-main"></div>
+                    <div className="lazyload-bg lazyload-fade lazy-main"></div>
                         <hr/>
                         <div className="lazy-sub">
-                            <div className="animated-background progressbar-1"></div>
-                            <div className="animated-background progressbar-2"></div>
-                            <div className="animated-background progressbar-3"></div>
+                            <div className="lazyload-bg lazyload-fade progressbar-1"></div>
+                            <div className="lazyload-bg lazyload-fade progressbar-2"></div>
+                            <div className="lazyload-bg lazyload-fade progressbar-3"></div>
                         </div>
                     </div>
                 </div>
-                <div className="lazy-load small-6 medium-4 large-3 xlarge-3 column hide-for-small-only">
+                <div className="lazy-load small-6 medium-4 large-3 xlarge-3 column lazy-load-3 hide-for-small-only">
                 <div className="lazy-border">
-                    <div className="animated-background lazy-main"></div>
+                    <div className="lazyload-bg lazyload-fade lazy-main"></div>
                         <hr/>
                         <div className="lazy-sub">
-                            <div className="animated-background progressbar-1"></div>
-                            <div className="animated-background progressbar-2"></div>
-                            <div className="animated-background progressbar-3"></div>
+                            <div className="lazyload-bg lazyload-fade progressbar-1"></div>
+                            <div className="lazyload-bg lazyload-fade progressbar-2"></div>
+                            <div className="lazyload-bg lazyload-fade progressbar-3"></div>
                         </div>
                     </div>
                 </div>
-                <div className="lazy-load small-6 medium-4 large-3 xlarge-3 column hide-for-small-only hide-for-medium-only">
+                <div className="lazy-load small-6 medium-4 large-3 xlarge-3 column lazy-load-4 hide-for-small-only hide-for-medium-only">
                     <div className="lazy-border">
-                        <div className="animated-background lazy-main"></div>
+                        <div className="lazyload-bg lazyload-fade lazy-main"></div>
                         <hr/>
                         <div className="lazy-sub">
-                            <div className="animated-background progressbar-1"></div>
-                            <div className="animated-background progressbar-2"></div>
-                            <div className="animated-background progressbar-3"></div>
+                            <div className="lazyload-bg lazyload-fade progressbar-1"></div>
+                            <div className="lazyload-bg lazyload-fade progressbar-2"></div>
+                            <div className="lazyload-bg lazyload-fade progressbar-3"></div>
                         </div>
                     </div>
                 </div>
