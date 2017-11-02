428b1e22bc04fe9c49c32c08869886a6094e2aff
Bala M <bmp@contractor.jcp.com>
2017-11-02 17:32:21 +0530

    code cleanup - removed the unwanted codes

--- src/js/components/content-loaders/content-loaders.js
+++ src/js/components/content-loaders/content-loaders.js
@@ -3,153 +3,175 @@ import $ from 'jquery'
 
 class ContentLoaders extends Component {
 
-componentDidMount() {
-    $('.loader-count').each(function () {
-        $(this).prop('Counter',0).animate({
-            Counter: $(this).text()
-        }, {
-            duration: 2000,
-            step: function (now) {
-                $(this).text(Math.ceil(now));
-            }
+    componentDidMount() {
+        $('.loader-count').each(function () {
+            $(this).prop('Counter', 0).animate({
+                Counter: $(this).text()
+            }, {
+                    duration: 2000,
+                    step: function (now) {
+                        $(this).text(Math.ceil(now));
+                    }
+                });
         });
-    });
-    setTimeout(function() {$(".lazy-load-1").addClass("lazy-count");}, 500);
-    setTimeout(function() {$(".lazy-load-2").addClass("lazy-count");}, 1200);
-    setTimeout(function() {$(".lazy-load-3").addClass("lazy-count");}, 1900);
-    setTimeout(function() {$(".lazy-load-4").addClass("lazy-count");}, 2600);
-}
+        $(document).ready(function () {
+            function isScrolledIntoView(elem) {
+                var docViewTop = $(window).scrollTop();
+                var docViewBottom = docViewTop + $(window).height();
+                var elemTop = $(elem).offset().top;
+                var elemBottom = elemTop + $(elem).height();
+                return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+            }
+
+            $(window).scroll(function () {
+                $('.lazy-load-container').each(function () {
+                    if (isScrolledIntoView(this) === true) {
+                        setTimeout(function () { $(".lazy-load-1").addClass("lazy-count"); }, 500);
+                        setTimeout(function () { $(".lazy-load-2").addClass("lazy-count"); }, 1200);
+                        setTimeout(function () { $(".lazy-load-3").addClass("lazy-count"); }, 1900);
+                        setTimeout(function () { $(".lazy-load-4").addClass("lazy-count"); }, 2600);
+                    } else {
+                        setTimeout(function () { $(".lazy-load-1").removeClass("lazy-count"); });
+                        setTimeout(function () { $(".lazy-load-2").removeClass("lazy-count"); });
+                        setTimeout(function () { $(".lazy-load-3").removeClass("lazy-count"); });
+                        setTimeout(function () { $(".lazy-load-4").removeClass("lazy-count"); });
+                    }
+                });
+
+            });
+        });
+    }
     render() {
-    return (
-<div>
-    <div className="row">
-        <div className="small-12 columns">
-            <h1>Content Loaders</h1>
-            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
-            <hr />
-        </div>
-    </div>
-    
-    <div className="row" id="types">
-        <div className="small-12 columns">
-            <h3>Types</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <div className="small-12 medium-12 columns">
-                <div className="small-12 medium-4 columns">
-                <div className="loader-container">
-                <div className="loader-spin">
-                    <div className="loader-logo">
-                        <div className="loader-circle">
-                            <svg className="loader-circle-spinner" viewBox="0 0 500 500">
-                                <circle cx="250" cy="250" r="239"> </circle>
-                            </svg>
+        return (
+            <div>
+                <div className="row">
+                    <div className="small-12 columns">
+                        <h1>Content Loaders</h1>
+                        <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+                        <hr />
+                    </div>
+                </div>
+
+                <div className="row" id="types">
+                    <div className="small-12 columns">
+                        <h3>Types</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div className="small-12 medium-12 columns">
+                            <div className="small-12 medium-4 columns">
+                                <div className="loader-container">
+                                    <div className="loader-spin">
+                                        <div className="loader-logo">
+                                            <div className="loader-circle">
+                                                <svg className="loader-circle-spinner" viewBox="0 0 500 500">
+                                                    <circle cx="250" cy="250" r="239"> </circle>
+                                                </svg>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="small-12 medium-8 columns">
+                                <h4>Page</h4>
+                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec ullamcorper nulla non metus auctor fringilla. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. <br /><br /><br /><br /></p>
+                            </div>
+                        </div>
+                        <div className="small-12 medium-12 columns">
+                            <div className="small-12 medium-4 columns position-rel">
+                                <div className="loader-checkmark-container">
+                                    <svg className="checkmark" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                                        <circle className="checkmark-circle" fill="none" cx="85.6" cy="80.6" r="30" transform="rotate(-45, 93, 93)" />
+                                        <polyline className="checkmark-icon" fill="none" points="93,79.8 77.1,103.4 68,95.4" />
+                                    </svg>
+                                </div>
+                                <div className="loader-count-container">
+                                    <span className="loader-count">100</span>
+                                    <span>%</span>
+                                </div>
+                            </div>
+                            <div className="small-12 medium-8 columns">
+                                <h4>Progress</h4>
+                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec ullamcorper nulla non metus auctor fringilla. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. <br /><br /><br /><br /></p>
+                            </div>
+                        </div>
+                        <div className="small-12 medium-12 columns section-container">
+                            <div className="small-12 medium-4 columns">
+                                <div className="shimmer-bg progressbar-1"></div>
+                                <div className="shimmer-bg progressbar-2"></div>
+                                <div className="shimmer-bg progressbar-3"></div>
+                            </div>
+                            <div className="small-12 medium-8 columns">
+                                <h4>Section</h4>
+                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec ullamcorper nulla non metus auctor fringilla. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. <br /><br /></p>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <hr />
+                <div className="row" id="lazy-load">
+                    <div className="small-12 columns">
+                        <h3>Lazy Load</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div className="lazy-load-container columns">
+                            <div className="lazy-load small-6 medium-4 large-3 xlarge-3 column lazy-load-1">
+                                <div className="lazy-border">
+                                    <div className="lazyload-bg lazyload-fade lazy-main"></div>
+                                    <hr />
+                                    <div className="lazy-sub">
+                                        <div className="lazyload-bg lazyload-fade progressbar-1"></div>
+                                        <div className="lazyload-bg lazyload-fade progressbar-2"></div>
+                                        <div className="lazyload-bg lazyload-fade progressbar-3"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="lazy-load small-6 medium-4 large-3 xlarge-3 column lazy-load-2">
+                                <div className="lazy-border">
+                                    <div className="lazyload-bg lazyload-fade lazy-main"></div>
+                                    <hr />
+                                    <div className="lazy-sub">
+                                        <div className="lazyload-bg lazyload-fade progressbar-1"></div>
+                                        <div className="lazyload-bg lazyload-fade progressbar-2"></div>
+                                        <div className="lazyload-bg lazyload-fade progressbar-3"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="lazy-load small-6 medium-4 large-3 xlarge-3 column lazy-load-3 hide-for-small-only">
+                                <div className="lazy-border">
+                                    <div className="lazyload-bg lazyload-fade lazy-main"></div>
+                                    <hr />
+                                    <div className="lazy-sub">
+                                        <div className="lazyload-bg lazyload-fade progressbar-1"></div>
+                                        <div className="lazyload-bg lazyload-fade progressbar-2"></div>
+                                        <div className="lazyload-bg lazyload-fade progressbar-3"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="lazy-load small-6 medium-4 large-3 xlarge-3 column lazy-load-4 hide-for-small-only hide-for-medium-only">
+                                <div className="lazy-border">
+                                    <div className="lazyload-bg lazyload-fade lazy-main"></div>
+                                    <hr />
+                                    <div className="lazy-sub">
+                                        <div className="lazyload-bg lazyload-fade progressbar-1"></div>
+                                        <div className="lazyload-bg lazyload-fade progressbar-2"></div>
+                                        <div className="lazyload-bg lazyload-fade progressbar-3"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                </div>
-                <div className="small-12 medium-8 columns">
-                    <h4>Page</h4>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec ullamcorper nulla non metus auctor fringilla. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. <br /><br /><br /><br /></p>
+                <br /><br />
+                <hr />
+                <br />
+                <div className="bala"></div>
+                <div className="row created-by">
+                    <div className="small-12 columns">
+                        <p>Created by: <strong>Tony Stark</strong> and <strong>Bruce Banner</strong>
+                            <br /> Latest update: 04/05/2017</p>
+                    </div>
                 </div>
             </div>
-            <div className="small-12 medium-12 columns">
-                <div className="small-12 medium-4 columns position-rel">
-                    <div className="loader-checkmark-container">
-                        <svg className="checkmark" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                            <circle className="checkmark-circle" fill="none" cx="85.6" cy="80.6" r="30" transform="rotate(-45, 93, 93)"/>
-                            <polyline className="checkmark-icon" fill="none" points="93,79.8 77.1,103.4 68,95.4"/>
-                        </svg>
-                    </div>
-                    <div className="loader-count-container">
-                        <span className="loader-count">100</span>
-                        <span>%</span>
-                    </div>
-                </div>
-                <div className="small-12 medium-8 columns">
-                    <h4>Progress</h4>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec ullamcorper nulla non metus auctor fringilla. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. <br /><br /><br /><br /></p>
-                </div>
-            </div>
-            <div className="small-12 medium-12 columns section-container">
-                <div className="small-12 medium-4 columns">
-                    <div className="shimmer-bg progressbar-1"></div>
-                    <div className="shimmer-bg progressbar-2"></div>
-                    <div className="shimmer-bg progressbar-3"></div>
-                </div>
-                <div className="small-12 medium-8 columns">
-                    <h4>Section</h4>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec ullamcorper nulla non metus auctor fringilla. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. <br /><br /></p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <hr />
-    
-    <div className="row" id="lazy-load">
-        <div className="small-12 columns">
-            <h3>Lazy Load</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <div className="lazy-load-container columns">
-                <div className="lazy-load small-6 medium-4 large-3 xlarge-3 column lazy-load-1">
-                    <div className="lazy-border">
-                        <div className="lazyload-bg lazyload-fade lazy-main"></div>
-                        <hr/>
-                        <div className="lazy-sub">
-                            <div className="lazyload-bg lazyload-fade progressbar-1"></div>
-                            <div className="lazyload-bg lazyload-fade progressbar-2"></div>
-                            <div className="lazyload-bg lazyload-fade progressbar-3"></div>
-                        </div>
-                    </div>
-                </div>
-                <div className="lazy-load small-6 medium-4 large-3 xlarge-3 column lazy-load-2">
-                <div className="lazy-border">
-                    <div className="lazyload-bg lazyload-fade lazy-main"></div>
-                        <hr/>
-                        <div className="lazy-sub">
-                            <div className="lazyload-bg lazyload-fade progressbar-1"></div>
-                            <div className="lazyload-bg lazyload-fade progressbar-2"></div>
-                            <div className="lazyload-bg lazyload-fade progressbar-3"></div>
-                        </div>
-                    </div>
-                </div>
-                <div className="lazy-load small-6 medium-4 large-3 xlarge-3 column lazy-load-3 hide-for-small-only">
-                <div className="lazy-border">
-                    <div className="lazyload-bg lazyload-fade lazy-main"></div>
-                        <hr/>
-                        <div className="lazy-sub">
-                            <div className="lazyload-bg lazyload-fade progressbar-1"></div>
-                            <div className="lazyload-bg lazyload-fade progressbar-2"></div>
-                            <div className="lazyload-bg lazyload-fade progressbar-3"></div>
-                        </div>
-                    </div>
-                </div>
-                <div className="lazy-load small-6 medium-4 large-3 xlarge-3 column lazy-load-4 hide-for-small-only hide-for-medium-only">
-                    <div className="lazy-border">
-                        <div className="lazyload-bg lazyload-fade lazy-main"></div>
-                        <hr/>
-                        <div className="lazy-sub">
-                            <div className="lazyload-bg lazyload-fade progressbar-1"></div>
-                            <div className="lazyload-bg lazyload-fade progressbar-2"></div>
-                            <div className="lazyload-bg lazyload-fade progressbar-3"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>    
-    <br/><br/>
-    <hr />
-    <br/>
-    <div className="row created-by">
-        <div className="small-12 columns">
-            <p>Created by: <strong>Tony Stark</strong> and <strong>Bruce Banner</strong>
-            <br /> Latest update: 04/05/2017</p>
-        </div>
-    </div>
-</div>
-    );
-  }
+        );
+    }
 }
 
 export default ContentLoaders;
--- src/js/components/forms/forms.js
+++ src/js/components/forms/forms.js
@@ -3,57 +3,58 @@ import $ from 'jquery'
 
 class Forms extends Component {
 
-componentDidMount() {
-    $('select').change(function(){
-        $('.select-label').removeClass('hide');
-    });
-}
+    componentDidMount() {
+        $('select').change(function () {
+            var selectedClass = $(".select select");
+            selectedClass.val() == '' ? $('.select-label').addClass('hide') : $('.select-label').removeClass('hide');
+        });
+    }
     render() {
-    return (
-<div>
-    <div className="row">
-        <div className="small-12 columns">
-            <h1>Forms</h1>
-            <h2>Vestibulum id ligula porta felis euismod semper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</h2>
-            <hr />
-        </div>
-    </div>
-    <div className="row" id="appearance">
-        <div className="small-12 columns">
-            <h3>Appearance</h3>
-            <p>Vestibulum id ligula porta felis euismod semper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. </p>
-            <div className="small-12 medium-12 columns">
-                <form>
-                    <div className="small-12 medium-4 columns select">
-                        <span className="select-label hide">Month</span>
-                        <select className="" name="select_month" >
-                        <option value=''>Select Month</option>
-                        <option value='1'>Janaury</option>
-                        <option value='2'>February</option>
-                        <option value='3'>March</option>
-                        <option value='4'>April</option>
-                        <option value='5'>May</option>
-                        <option value='6'>June</option>
-                        <option value='7'>July</option>
-                        <option value='8'>August</option>
-                        <option value='9'>September</option>
-                        <option value='10'>October</option>
-                        <option value='11'>November</option>
-                        <option value='12'>December</option>
-                        </select>
+        return (
+            <div>
+                <div className="row">
+                    <div className="small-12 columns">
+                        <h1>Forms</h1>
+                        <h2>Vestibulum id ligula porta felis euismod semper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</h2>
+                        <hr />
                     </div>
-                </form>
+                </div>
+                <div className="row" id="appearance">
+                    <div className="small-12 columns">
+                        <h3>Appearance</h3>
+                        <p>Vestibulum id ligula porta felis euismod semper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. </p>
+                        <div className="small-12 medium-12 columns">
+                            <form>
+                                <div className="small-12 medium-4 columns select">
+                                    <span className="select-label hide">Month</span>
+                                    <select className="" name="select_month" >
+                                        <option value=''>Select Month</option>
+                                        <option value='1'>Janaury</option>
+                                        <option value='2'>February</option>
+                                        <option value='3'>March</option>
+                                        <option value='4'>April</option>
+                                        <option value='5'>May</option>
+                                        <option value='6'>June</option>
+                                        <option value='7'>July</option>
+                                        <option value='8'>August</option>
+                                        <option value='9'>September</option>
+                                        <option value='10'>October</option>
+                                        <option value='11'>November</option>
+                                        <option value='12'>December</option>
+                                    </select>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="small-12 medium-8 columns">
+                            <h4>Text Field</h4>
+                            <p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nullam quis risus.<br /><br /><br /><br /></p>
+                        </div>
+                    </div>
+                </div>
+                <hr />
             </div>
-            <div className="small-12 medium-8 columns">
-                <h4>Text Field</h4>
-                <p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nullam quis risus.<br /><br /><br /><br /></p>
-            </div>
-        </div>
-    </div>
-    <hr />
-</div>
-    );
-  }
+        );
+    }
 }
 
 export default Forms;
