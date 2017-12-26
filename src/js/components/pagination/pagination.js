5756e1636044c09e20897771de1a157862d7944c
Bala M <bmp@contractor.jcp.com>
2017-12-26 17:04:04 +0530

    pagination update

--- src/js/components/pagination/pagination.js
+++ src/js/components/pagination/pagination.js
@@ -9,7 +9,61 @@ class Pagination extends Component {
     return (
       <div>
         <div className="row">
-          Pagination
+          <div className="paginiation-block">
+            <h1 class="title title-XL mrg-L">Pagination</h1>
+            <div className="pagination mrg-XXL">
+              <p className="std-txt-label">Selected</p>
+              <ul>
+                <li class="pagination-prev hide" aria-label="Prev"></li>
+                <li className="pagination-lbl pagination-active">1</li>
+                <li className="pagination-lbl">2</li>
+                <li className="pagination-lbl">3</li>
+                <li className="pagination-lbl">4</li>
+                <li class="pagination-elipsis">...</li>
+                <li class="pagination-lbl">99</li>
+                <li class="pagination-next" aria-label="Next"></li>
+              </ul>
+            </div>
+            <div className="pagination mrg-XXL">
+              <p className="std-txt-label">On Hover</p>
+              <ul>
+                <li class="pagination-prev" aria-label="Prev"></li>
+                <li className="pagination-lbl pagination-active">1</li>
+                <li className="pagination-lbl">2</li>
+                <li className="pagination-lbl">3</li>
+                <li className="pagination-lbl">4</li>
+                <li class="pagination-elipsis">...</li>
+                <li class="pagination-lbl">99</li>
+                <li class="pagination-next" aria-label="Next"></li>
+              </ul>
+            </div>
+            <div className="pagination mrg-XXL">
+              <p className="std-txt-label">Other pages selected</p>
+              <ul>
+                <li class="pagination-prev" aria-label="Prev"></li>
+                <li className="pagination-lbl pagination-active">1</li>
+                <li className="pagination-lbl">2</li>
+                <li className="pagination-lbl">3</li>
+                <li className="pagination-lbl">4</li>
+                <li class="pagination-elipsis">...</li>
+                <li class="pagination-lbl">99</li>
+                <li class="pagination-next" aria-label="Next"></li>
+              </ul>
+            </div>
+            <div className="pagination mrg-XXL">
+              <p className="std-txt-label">s</p>
+              <ul>
+                <li class="pagination-prev" aria-label="Prev"></li>
+                <li className="pagination-lbl pagination-active">1</li>
+                <li className="pagination-lbl">2</li>
+                <li className="pagination-lbl">3</li>
+                <li className="pagination-lbl">4</li>
+                <li class="pagination-elipsis">...</li>
+                <li class="pagination-lbl">99</li>
+                <li class="pagination-next" aria-label="Next"></li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     );
