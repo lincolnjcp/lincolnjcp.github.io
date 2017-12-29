2f618e5d1f15711f9787af679b2ebe1051b6c6e1
Bala M <bmp@contractor.jcp.com>
2017-12-29 20:01:26 +0530

    coupon updates

--- src/js/components/coupons/coupons.js
+++ src/js/components/coupons/coupons.js
@@ -8,21 +8,189 @@ class Coupons extends Component {
   render() {
     return (
       <div>
-        <div className="row">
+        <div className="row flex-box">
           <div className="coupon-block flex-grid sm12 md6 lg4 xl3">
             <div className="coupon-list">
-              <span className="std-txt std-txt-S">Online Only</span>
-              <h2 className="title-L">Extra 30% OFF $100+</h2>
-              <p className="title title-S">select apparel, shoes, accessories, fine jewelry & home</p>
-              <h4 className="title-M">Or Extra 25% OFF Under $100</h4>
-              <p className="title-S">with any method of payment</p>
-              <span className="std-txt-S">Expires: 11/05/2017</span>
-              <p><span>Code: </span><span className="std-txt-S">SHOPNOW4</span></p>
-              <a href="javascript:void();" className="btn btn-S btn-primary apply-btn btn-full">Apply</a>
-              <p>
+              <span className="coupon-badge title mrg-S">SALE ENDS IN 2HRS</span>
+              <span className="std-txt std-txt-S mrg-S">Online Only</span>
+              <div className="coupon-primary-offer mrg-M">
+                <h2 className="title-L">Extra 30% OFF $100+</h2>
+                <p className="std-txt std-txt-S">select apparel, shoes, accessories, fine jewelry & home</p>
+              </div>
+              <div className="coupon-secondary-offer mrg-M">
+                <h4 className="title-M">Or Extra 25% OFF Under $100</h4>
+                <p className="std-txt std-txt-S">with any method of payment</p>
+              </div>
+              <div className="coupon-code mrg-S">
+                <span className="std-txt std-txt-S">Expires: 11/05/2017</span>
+                <p><span>Code: </span><span className="title-S title">SHOPNOW4</span></p>
+              </div>
+              <a href="javascript:void();" className="btn btn-S btn-primary apply-btn btn-full mrg-S">Apply</a>
+              <div className="coupon-see-details">
                 <a href="javascript:void(0)">shop home</a>
                 <a href="javascript:void(0)">see details</a>
-              </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="coupon-block flex-grid sm12 md6 lg4 xl3">
+            <div className="coupon-list">
+              <span className="coupon-badge title mrg-S">SALE ENDS IN 2HRS</span>
+              <span className="std-txt std-txt-S mrg-S">Online Only</span>
+              <div className="coupon-primary-offer mrg-M">
+                <h2 className="title-L">Extra 30% OFF $100+</h2>
+                <p className="std-txt std-txt-S">select apparel, shoes, accessories, fine jewelry & home</p>
+              </div>
+              <div className="coupon-secondary-offer mrg-M">
+                <h4 className="title-M">Or Extra 25% OFF Under $100</h4>
+                <p className="std-txt std-txt-S">with any method of payment</p>
+              </div>
+              <div className="coupon-code mrg-S">
+                <span className="std-txt std-txt-S">Expires: 11/05/2017</span>
+                <p><span>Code: </span><span className="title-S title">SHOPNOW4</span></p>
+              </div>
+              <a href="javascript:void();" className="btn btn-S btn-primary apply-btn btn-full mrg-S">Apply</a>
+              <div className="coupon-see-details">
+                <a href="javascript:void(0)">shop home</a>
+                <a href="javascript:void(0)">see details</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="coupon-block flex-grid sm12 md6 lg4 xl3">
+            <div className="coupon-list">
+              <span className="coupon-badge title mrg-S">SALE ENDS IN 2HRS</span>
+              <span className="std-txt std-txt-S mrg-S">Online Only</span>
+              <div className="coupon-primary-offer mrg-M">
+                <h2 className="title-L">Extra 30% OFF $100+</h2>
+                <p className="std-txt std-txt-S">select apparel, shoes, accessories, fine jewelry & home</p>
+              </div>
+              <a href="javascript:void();" className="btn btn-S btn-primary apply-btn btn-full mrg-S">Apply</a>
+              <div className="coupon-see-details">
+                <a href="javascript:void(0)">shop home</a>
+                <a href="javascript:void(0)">see details</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="coupon-block flex-grid sm12 md6 lg4 xl3">
+            <div className="coupon-list">
+              <span className="coupon-badge title mrg-S">SALE ENDS IN 2HRS</span>
+              <span className="std-txt std-txt-S mrg-S">Online Only</span>
+              <div className="coupon-primary-offer mrg-M">
+                <h2 className="title-L">Extra 30% OFF $100+</h2>
+                <p className="std-txt std-txt-S">select apparel, shoes, accessories, fine jewelry & home</p>
+              </div>
+              <div className="coupon-secondary-offer mrg-M">
+                <h4 className="title-M">Or Extra 25% OFF Under $100</h4>
+                <p className="std-txt std-txt-S">with any method of payment</p>
+              </div>
+              <div className="coupon-code mrg-S">
+                <span className="std-txt std-txt-S">Expires: 11/05/2017</span>
+                <p><span>Code: </span><span className="title-S title">SHOPNOW4</span></p>
+              </div>
+              <a href="javascript:void();" className="btn btn-S btn-primary apply-btn btn-full mrg-S">Apply</a>
+              <a href="javascript:void();" className="btn btn-S btn-secondary apply-btn btn-full mrg-S print-barcode">Print Barcode</a>
+              <div className="coupon-see-details">
+                <a href="javascript:void(0)">shop home</a>
+                <a href="javascript:void(0)">see details</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="coupon-block flex-grid sm12 md6 lg4 xl3">
+            <div className="coupon-list">
+              <span className="coupon-badge title mrg-S">SALE ENDS IN 2HRS</span>
+              <span className="std-txt std-txt-S mrg-S">Online Only</span>
+              <div className="coupon-primary-offer mrg-M">
+                <h2 className="title-L">Extra 30% OFF $100+</h2>
+                <p className="std-txt std-txt-S">select apparel, shoes, accessories, fine jewelry & home</p>
+              </div>
+              <div className="coupon-secondary-offer mrg-M">
+                <h4 className="title-M">Or Extra 25% OFF Under $100</h4>
+                <p className="std-txt std-txt-S">with any method of payment</p>
+              </div>
+              <div className="coupon-code mrg-S">
+                <span className="std-txt std-txt-S">Expires: 11/05/2017</span>
+                <p><span>Code: </span><span className="title-S title">SHOPNOW4</span></p>
+              </div>
+              <a href="javascript:void();" className="btn btn-S btn-primary apply-btn btn-full mrg-S">Apply</a>
+              <div className="coupon-see-details">
+                <a href="javascript:void(0)">shop home</a>
+                <a href="javascript:void(0)">see details</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="coupon-block flex-grid sm12 md6 lg4 xl3">
+            <div className="coupon-list">
+              <span className="coupon-badge title mrg-S">SALE ENDS IN 2HRS</span>
+              <span className="std-txt std-txt-S mrg-S">Online Only</span>
+              <div className="coupon-primary-offer mrg-M">
+                <h2 className="title-L">Extra 30% OFF $100+</h2>
+                <p className="std-txt std-txt-S">select apparel, shoes, accessories, fine jewelry & home</p>
+              </div>
+              <div className="coupon-secondary-offer mrg-M">
+                <h4 className="title-M">Or Extra 25% OFF Under $100</h4>
+                <p className="std-txt std-txt-S">with any method of payment</p>
+              </div>
+              <div className="coupon-code mrg-S">
+                <span className="std-txt std-txt-S">Expires: 11/05/2017</span>
+                <p><span>Code: </span><span className="title-S title">SHOPNOW4</span></p>
+              </div>
+              <a href="javascript:void();" className="btn btn-S btn-primary apply-btn btn-full mrg-S">Apply</a>
+              <div className="coupon-see-details">
+                <a href="javascript:void(0)">shop home</a>
+                <a href="javascript:void(0)">see details</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="coupon-block flex-grid sm12 md6 lg4 xl3">
+            <div className="coupon-list">
+              <span className="coupon-badge title mrg-S">SALE ENDS IN 2HRS</span>
+              <span className="std-txt std-txt-S mrg-S">Online Only</span>
+              <div className="coupon-primary-offer mrg-M">
+                <h2 className="title-L">Extra 30% OFF $100+</h2>
+                <p className="std-txt std-txt-S">select apparel, shoes, accessories, fine jewelry & home</p>
+              </div>
+              <div className="coupon-secondary-offer mrg-M">
+                <h4 className="title-M">Or Extra 25% OFF Under $100</h4>
+                <p className="std-txt std-txt-S">with any method of payment</p>
+              </div>
+              <div className="coupon-code mrg-S">
+                <span className="std-txt std-txt-S">Expires: 11/05/2017</span>
+                <p><span>Code: </span><span className="title-S title">SHOPNOW4</span></p>
+              </div>
+              <a href="javascript:void();" className="btn btn-S btn-primary apply-btn btn-full mrg-S">Apply</a>
+              <div className="coupon-see-details">
+                <a href="javascript:void(0)">shop home</a>
+                <a href="javascript:void(0)">see details</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="coupon-block flex-grid sm12 md6 lg4 xl3">
+            <div className="coupon-list">
+              <span className="coupon-badge title mrg-S">SALE ENDS IN 2HRS</span>
+              <span className="std-txt std-txt-S mrg-S">Online Only</span>
+              <div className="coupon-primary-offer mrg-M">
+                <h2 className="title-L">Extra 30% OFF $100+</h2>
+                <p className="std-txt std-txt-S">select apparel, shoes, accessories, fine jewelry & home</p>
+              </div>
+              <div className="coupon-secondary-offer mrg-M">
+                <h4 className="title-M">Or Extra 25% OFF Under $100</h4>
+                <p className="std-txt std-txt-S">with any method of payment</p>
+              </div>
+              <div className="coupon-code mrg-S">
+                <span className="std-txt std-txt-S">Expires: 11/05/2017</span>
+                <p><span>Code: </span><span className="title-S title">SHOPNOW4</span></p>
+              </div>
+              <a href="javascript:void();" className="btn btn-S btn-primary apply-btn btn-full mrg-S">Apply</a>
+              <div className="coupon-see-details">
+                <a href="javascript:void(0)">shop home</a>
+                <a href="javascript:void(0)">see details</a>
+              </div>
             </div>
           </div>
         </div>
