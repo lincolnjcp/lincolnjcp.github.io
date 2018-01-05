4d7a4d8f1feb3c385ed370c64a58a548df24ce10
Bala M <bmp@contractor.jcp.com>
2018-01-05 12:39:52 +0530

    footer initial commit

---
+++ src/js/components/footer/footer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+
+class Footer extends Component {
+
+    componentDidMount() {
+    }
+    render() {
+        return (
+            <div>
+                <div className="row">
+                    
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Footer;
