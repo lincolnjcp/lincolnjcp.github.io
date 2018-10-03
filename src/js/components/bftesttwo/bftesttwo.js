5f188edeb7f5b35a857049221aea4f28fddf42c4
Sanju Balakrishnan <sbalakr5@contractor.jcp.com>
2018-10-03 19:46:32 +0530

    Build Changes

--- src/js/components/bftesttwo/bftesttwo.js
+++ src/js/components/bftesttwo/bftesttwo.js
@@ -9,7 +9,50 @@ class BFTestTwo extends Component {
   render() {
     return (
       <div>
-        
+        <div class="viz-nav viz-nav-squre mrg-btm-XXL">
+            <ul>
+              <li class="mrg-btm-XL mrg-lft-XL">
+                <a href="#">
+                  <div class="viz-nav-img mrg-btm-S">
+                    <img src="https://m.jcpenney.com/tablet/images/wmns-coats.jpg" alt="product-image" />
+                  </div>
+                  <h2 class="title title-M mrg-btm-XS algn-mid">Bed & Bath</h2>
+                </a>
+              </li>
+              <li class="mrg-btm-XL mrg-lft-XL">
+                <a href="#">
+                  <div class="viz-nav-img mrg-btm-S">
+                    <img src="https://m.jcpenney.com/tablet/images/wmns_tops.jpg" alt="product-image" />
+                  </div>
+                  <h2 class="title title-M mrg-btm-XS algn-mid">Jeans</h2>
+                </a>
+              </li>
+              <li class="mrg-btm-XL mrg-lft-XL">
+                <a href="#">
+                  <div class="viz-nav-img mrg-btm-S">
+                    <img src="https://m.jcpenney.com/tablet/images/wmns_jeans.jpg" alt="product-image" />
+                  </div>
+                  <h2 class="title title-M mrg-btm-XS algn-mid">Pants</h2>
+                </a>
+              </li>
+              <li class="mrg-btm-XL mrg-lft-XL">
+                <a href="#">
+                  <div class="viz-nav-img mrg-btm-S">
+                    <img src="https://m.jcpenney.com/tablet/images/wmns_blazer.jpg" alt="product-image" />
+                  </div>
+                  <h2 class="title title-M mrg-btm-XS algn-mid">Dresses</h2>
+                </a>
+              </li>
+              <li class="mrg-btm-XL mrg-lft-XL">
+                <a href="#">
+                  <div class="viz-nav-img mrg-btm-S">
+                    <img src="https://m.jcpenney.com/tablet/images/womens%20scrubs%20icon.jpg" alt="product-image" />
+                  </div>
+                  <h2 class="title title-M mrg-btm-XS algn-mid">Cropped Pants</h2>
+                </a>
+              </li>
+            </ul>
+          </div>
       </div>
     );
   }
