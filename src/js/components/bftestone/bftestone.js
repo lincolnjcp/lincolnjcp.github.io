3c1b7869fb209c0f2d049a3fc2e9f19792ff4535
Mahendran Arunkumar <amahend1@contractor.jcp.com>
2018-10-03 19:45:30 +0530

    bf test one style applied

--- src/js/components/bftestone/bftestone.js
+++ src/js/components/bftestone/bftestone.js
@@ -8,9 +8,102 @@ class BFTestOne extends Component {
 
   render() {
     return (
-      <div>
-        Bf test1
-      </div>
+      <div className="row">
+                <Header />
+                <div className="lg12 sm12 md12 xl12">
+                    <h1 className="title title-XL fl-left black-friday-title">BLACK FRIDAY DEALS FOR HOME</h1>
+                    <p className="std-txt std-txt-S fl-right black-friday-std-txt"><a href="javascript:void(0);">Shop All</a></p>
+                </div>
+                <div className="row">
+                    <div className="lg12 sm12 md12 xl12">
+                        <div className="product-card-wrapper black-friday-wrapper">
+                        <a href="javascript:void(0);" className="black-friday-container">
+                            <div className="product-card product-card-pane-grid black-friday-pane-grid">
+                                <div className="black-friday-card-inner black-friday-product1">
+                                        <div className="product-card-details">
+                                        <div className="price price-M black-friday-price-L color-penneyred">14<sup class="sup-text">99</sup></div>
+                                        <div class="product-card-name-wrap">
+                                        <div className="black-friday-card-name">Quinto or Soleil 16-pc Dinnerware set; service for 4 ›
+                                        <span className="black-friday-price">reg. $50</span></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            </a>
+                            <a href="javascript:void(0);" className="black-friday-container">
+                            <div className="product-card product-card-pane-grid black-friday-pane-grid">
+                                <div className="black-friday-card-inner black-friday-product2">
+                                        <div className="product-card-details">
+                                        <div className="price price-M color-penneyred">14<sup class="sup-text">99</sup></div>
+                                        <div class="product-card-name-wrap">
+                                        <div className="black-friday-card-name">16-pc Luminarc Glassware set ›
+                                        <span className="black-friday-price">reg. $50</span></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            </a>
+                            <a href="javascript:void(0);" className="black-friday-container">
+                            <div className="product-card product-card-pane-grid black-friday-pane-grid">
+                                <div className="black-friday-card-inner black-friday-product3">
+                                        <div className="product-card-details">
+                                        <div className="price price-M color-penneyred">2<sup class="sup-text">99</sup></div>
+                                        <div class="product-card-name-wrap">
+                                        <div className="black-friday-card-name">Home Expressions ombre stripe or solid bath towel ›
+                                        <span className="black-friday-price">reg. $10</span>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            </a>
+                            <a href="javascript:void(0);" className="black-friday-container">
+                            <div className="product-card product-card-pane-grid black-friday-pane-grid">
+                                <div className="black-friday-card-inner black-friday-product4">
+                                        <div className="product-card-details">
+                                        <div className="price price-M color-penneyred">8<sup class="sup-text">99</sup></div>
+                                        <div class="product-card-name-wrap">
+                                        <div className="black-friday-card-name">Mermaid Sequin Pillow ›
+                                        <span className="black-friday-price">reg. $27</span>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                </div>
+                                </a>
+                                <a href="javascript:void(0);" className="black-friday-container">
+                                <div className="product-card product-card-pane-grid black-friday-pane-grid">
+                                <div className="black-friday-card-inner black-friday-product5">
+                                        <div className="product-card-details">
+                                        <div className="price price-M color-penneyred">8<sup class="sup-text">99</sup></div>
+                                        <div class="product-card-name-wrap">
+                                        <div className="black-friday-card-name">Oneida 78-pc Flatware set ›
+                                        <span className="black-friday-price">reg. $200</span>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                </div>
+                                </a>
+                                <a href="javascript:void(0);" className="black-friday-container">
+                                <div className="product-card product-card-pane-grid black-friday-pane-grid">
+                                <div className="black-friday-card-inner black-friday-product6">
+                                        <div className="product-card-details">
+                                        <div className="price price-M color-penneyred">8<sup class="sup-text">99</sup></div>
+                                        <div class="product-card-name-wrap">
+                                        <div className="black-friday-card-name">Nutribullet Blender ›
+                                        <span className="black-friday-price">reg. $109</span>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                </div>
+                                </a>
+                            </div>
+                        </div>
+                </div>
+                <Footer />
+            </div>
     );
   }
 }
